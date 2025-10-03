4a2d24442b02c2086133149c08b3ab35126fc11c
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-03 15:05:52 +0300

    refactor theme data

--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -17,15 +17,17 @@ final ThemeData lightTheme = ThemeData(
     titleTextStyle: AppTextStyle.appBarStyle,
   ),
   inputDecorationTheme: InputDecorationTheme(
-    hintStyle:AppTextStyle.regular16 ,
-    filled: true,
+    hintStyle: AppTextStyle.regular16,
+    
+    filled: false,
     fillColor: LightColors.lightWhiteColor,
-    contentPadding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 14.h),
+    contentPadding: EdgeInsets.symmetric(vertical: 14.h, horizontal: 20.w),
     border: const OutlineInputBorder(),
-    enabledBorder:_buildTextFieldBorder() ,
+    enabledBorder: _buildTextFieldBorder(),
     focusedBorder: _buildTextFieldBorder(),
     errorBorder: _buildTextFieldBorder(),
-    focusedErrorBorder:_buildTextFieldBorder() 
+    errorStyle: AppTextStyle.regular16.copyWith(color: LightColors.redColor),
+    focusedErrorBorder: _buildTextFieldBorder(),
   ),
 
   elevatedButtonTheme: ElevatedButtonThemeData(
@@ -33,19 +35,23 @@ final ThemeData lightTheme = ThemeData(
       backgroundColor: LightColors.primaryColor,
       foregroundColor: Colors.white,
       textStyle: AppTextStyle.medium16.copyWith(fontSize: 14.sp),
-      padding: EdgeInsets.symmetric(horizontal: 153.w,vertical: 15.h),
-      fixedSize: Size(325.w, 50.h),
-    )
-  )
+      minimumSize: Size(325.w, 50.h),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.r)),
+    ),
+  ),
+  textSelectionTheme: const TextSelectionThemeData(
+    selectionHandleColor: LightColors.primaryColor,
+    cursorColor: LightColors.primaryColor,
+  ),
 );
 
-OutlineInputBorder _buildTextFieldBorder() {
+OutlineInputBorder _buildTextFieldBorder({double? borderWidth, Color? color}) {
   return OutlineInputBorder(
     borderRadius: BorderRadius.circular(10.r),
     borderSide: BorderSide(
-      width: 1.w,
-      color: LightColors.lightWhiteColor,
+      width: borderWidth ?? 1,
+      color: color ?? LightColors.lightWhiteColor,
       style: BorderStyle.solid,
-    )
+    ),
   );
 }
