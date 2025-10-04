5993cbd7227abdf51c117d71b04f4616dd3f12c9
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 21:58:24 +0300

    create auth repo to handle auth post request

---
+++ lib/features/auth/repo/auth_repo.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:marketi/core/networking/api_endpoints.dart';
+import 'package:marketi/core/networking/api_service.dart';
+import 'package:marketi/features/auth/models/auth_response_model.dart';
+
+class AuthRepo {
+  Future<Either<String, AuthResponseModel>> login({
+    required String userName,
+    required String password,
+  }) async {
+    try {
+      final Response response = await ApiService.postRequest(
+        endPoint: ApiEndPoints.login,
+        data: {"username": userName, "password": password},
+      );
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        AuthResponseModel authResponseModel = AuthResponseModel.fromJson(
+          response.data,
+        );
+        return Right(authResponseModel);
+      } else {
+        return Left(response.toString());
+      }
+    } catch (error) {
+      if (error is DioException) {
+        return Left(error.response.toString());
+      }
+      return Left(error.toString());
+    }
+  }
+}
