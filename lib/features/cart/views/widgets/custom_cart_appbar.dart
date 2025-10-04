8b45a000a35a2bfba90bcf30cff51a7971062518
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 16:19:11 +0300

    create custom cart appbar

---
+++ lib/features/cart/views/widgets/custom_cart_appbar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomCartAppbar extends StatelessWidget {
+  const CustomCartAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: const Text('My Cart'));
+    
+  }
+}
