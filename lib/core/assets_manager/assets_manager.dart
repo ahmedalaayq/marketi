0ef29f75949ce45be161969d37f69d5c473bb409
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 12:05:25 +0300

    create assets manager and add navbar icons svg

---
+++ lib/core/assets_manager/assets_manager.dart
@@ -0,0 +1,14 @@
+abstract class AssetsManager {
+  AssetsManager._();
+
+  
+  /// Base Assets Icons Path
+  static const String assetsIconsPath = 'assets/icons';
+  
+  /// Home NavBar Icon ///
+
+  
+  static const String homeNavBarIcon = '$assetsIconsPath/home.svg';
+  static const String cartNavBarIcon = '$assetsIconsPath/cart.svg';
+  static const String accountNavBarIcon = '$assetsIconsPath/account.svg';
+}
