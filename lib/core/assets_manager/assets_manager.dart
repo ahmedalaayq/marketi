027d64bbceaaeb86d7bc0bccf33fd11450b744c0
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 17:31:45 +0300

    refactor account view and create account view body

--- lib/core/assets_manager/assets_manager.dart
+++ lib/core/assets_manager/assets_manager.dart
@@ -1,19 +1,27 @@
 abstract class AssetsManager {
   AssetsManager._();
 
-  
   /// Base Assets Icons Path
   static const String assetsIconsPath = 'assets/icons';
-  
+
   /// Base Assets Images Path
   static const String assetsImagesPath = 'assets/images';
+
   /// Home NavBar Icon ///
   static const String homeNavBarIcon = '$assetsIconsPath/home.svg';
   static const String cartNavBarIcon = '$assetsIconsPath/cart.svg';
   static const String accountNavBarIcon = '$assetsIconsPath/account.svg';
-  /// Home Product Image /// 
-  static const String productImageShoes = '$assetsImagesPath/shoes.png';
-  static const String productImagePoloShirt = '$assetsImagesPath/polo_shirt.png';
 
-  
+  /// Home Product Image ///
+  static const String productImageShoes = '$assetsImagesPath/shoes.png';
+  static const String productImagePoloShirt =
+      '$assetsImagesPath/polo_shirt.png';
+
+  /// Account Icons
+
+  static const String accountBoxIcon = '$assetsIconsPath/Box.svg';
+  static const String accountDetailsIcon = '$assetsIconsPath/Details.svg';
+  static const String accountAddressBookIcon = '$assetsIconsPath/Address.svg';
+  static const String accountFAQsIcon = '$assetsIconsPath/Question.svg';
+  static const String accountHelpCenterIcon = '$assetsIconsPath/Headphones.svg';
 }
