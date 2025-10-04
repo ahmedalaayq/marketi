d99c51a0821a42e58d5c045c1008bce520214fa1
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 21:58:03 +0300

    finish api service and api end points

--- lib/core/networking/api_endpoints.dart
+++ lib/core/networking/api_endpoints.dart
@@ -1,3 +1,13 @@
 class ApiEndPoints {
-  static const String baseUrl = 'https://fakestoreapi.com/products';
+  static const String baseUrl = 'https://fakestoreapi.com';
+
+  static const String register = '/users';
+  static const String login = '/auth/login';
+
+  static const String products = '/products';
+  static const String categories = '/products/categories';
+
+  static const String categoryProduct = '/category';
+
+  static const String carts = '/carts';
 }
---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:marketi/core/networking/api_endpoints.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+abstract class ApiService {
+  static Dio? _dio;
+
+  static initDio() {
+    _dio ??= Dio(BaseOptions(baseUrl: ApiEndPoints.baseUrl));
+    _dio!.interceptors.add(PrettyDioLogger());
+  }
+
+  static Future<dynamic> getRequest({
+    required String endPoint,
+    Map<String, dynamic>? queryParamters,
+  }) async {
+    try {
+      final Response response = await _dio!.get(
+        endPoint,
+        queryParameters: {
+          ...queryParamters ?? {"Content-Type": 'application/json'},
+        },
+      );
+      return response;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  static Future<dynamic> postRequest({
+    required String endPoint,
+    Map<String, dynamic>? data,
+  }) async {
+    try {
+      final Response response = await _dio!.post(
+        endPoint,
+        data: {"Content-Type": 'application/json', ...data ?? {}},
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
