6f3d7163563e45be478d4a930c49f8a1a40a7522
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 21:58:54 +0300

    create auth response model to parsing response from login post request

--- lib/features/auth/views/widgets/custom_login_form.dart
+++ lib/features/auth/views/widgets/custom_login_form.dart
@@ -1,10 +1,15 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:marketi/core/router/app_routes.dart';
 import 'package:marketi/core/theme/light_colors.dart';
-import 'package:marketi/features/auth/views/widgets/custom_button.dart';
-import 'package:marketi/features/auth/views/widgets/custom_login_input_field.dart';
+import 'package:marketi/features/auth/models/auth_response_model.dart';
+import 'package:marketi/features/auth/repo/auth_repo.dart';
+
+import 'custom_button.dart';
+import 'custom_login_input_field.dart';
 
 class CustomLoginForm extends StatefulWidget {
   const CustomLoginForm({super.key});
@@ -14,19 +19,24 @@ class CustomLoginForm extends StatefulWidget {
 }
 
 class _CustomLoginFormState extends State<CustomLoginForm> {
-  late GlobalKey<FormState> _formKey;
+  late final GlobalKey<FormState> _formKey;
   late AutovalidateMode _autovalidateMode;
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
 
   @override
   void initState() {
     super.initState();
     _formKey = GlobalKey<FormState>();
     _autovalidateMode = AutovalidateMode.disabled;
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
   }
 
   @override
   void dispose() {
-    _formKey.currentState?.reset();
+    _emailController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
@@ -38,60 +48,80 @@ class _CustomLoginFormState extends State<CustomLoginForm> {
       child: Column(
         children: [
           CustomInputField(
-            inputLabel: 'Email Address',
-            hintText: 'Enter your email Address',
+            controller: _emailController,
+            inputLabel: 'User Name',
+            hintText: 'Enter your User Name',
             validator: (String? value) {
               if (value?.trim().isEmpty ?? true) {
-                return 'field email is required';
+                return 'field user name is required';
               }
-              final RegExp emailRegExp = RegExp(
-                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-              );
-
-              if (!emailRegExp.hasMatch(value!)) {
-                return "enter a valid email address";
-              }
+              // final RegExp emailRegExp = RegExp(
+              //   r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              // );
+              // if (!emailRegExp.hasMatch(value!)) {
+              //   return "enter a valid email address";
+              // }
               return null;
             },
           ),
           SizedBox(height: 16.h),
           CustomInputField(
-            validator: (String? value) {
-              if (value?.trim().isEmpty ?? false) {
-                return 'field password is required';
-              } else if (value!.length < 8) {
-                return 'password can\'t be less than 8 character';
-              }
-              return null;
-            },
+            controller: _passwordController,
             inputLabel: 'Password',
             hintText: 'Enter your password',
             suffixIcon: Icons.remove_red_eye,
+            validator: (String? value) {
+              if (value?.trim().isEmpty ?? true) {
+                return 'field password is required';
+              }
+              else if (value!.length < 6) {
+                return 'password can\'t be less than 6 characters';
+              }
+              return null;
+            },
           ),
-
           SizedBox(height: 51.h),
           Center(
             child: CustomButton(
               btnText: 'Sign in',
-              onPressed: () {
+              onPressed: () async {
                 if (_formKey.currentState?.validate() ?? false) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: LightColors.primaryColor,
-                      content: Row(
-                        children: [
-                          Expanded(child: Text('Validator Success')),
-                          Icon(Icons.error_outline, color: Colors.white),
-                        ],
-                      ),
-                    ),
+                  final response = await AuthRepo().login(
+                    userName: _emailController.text,
+                    password: _passwordController.text,
                   );
-                  GoRouter.of(context).push(AppRoutes.mainView);
-                } else {
-                  setState(() {
-                    _autovalidateMode = AutovalidateMode.always;
-                  });
+                  response.fold(
+                    (error) {
+                      return ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          behavior: SnackBarBehavior.floating,
+                          backgroundColor: Colors.red,
+                          content: Row(
+                            children: [
+                              const Icon(Icons.error, color: Colors.white),
+                              const SizedBox(width: 8),
+                              Expanded(child: Text(error)),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    (right) {
+                      GoRouter.of(context).push(AppRoutes.mainView);
+                      return ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          behavior: SnackBarBehavior.floating,
+                          backgroundColor: LightColors.primaryColor,
+                          content: Row(
+                            children: [
+                              Expanded(child: Text('Login Success')),
+                              Icon(Icons.check_circle, color: Colors.white),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
                 }
               },
             ),
