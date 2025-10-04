89831e996685b85c41d23ac8b50d83e632c852b6
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 15:22:49 +0300

    add cart view route

--- lib/core/router/router_manager.dart
+++ lib/core/router/router_manager.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:marketi/core/router/app_routes.dart';
 import 'package:marketi/features/auth/views/login_view.dart';
 import 'package:marketi/features/auth/views/sign_up_view.dart';
+import 'package:marketi/features/cart/views/cart_view.dart';
 import 'package:marketi/features/main/views/main_view.dart';
 import 'package:marketi/features/product_details/views/product_details_view.dart';
 
@@ -24,6 +25,10 @@ class RouterManager {
       GoRoute(
         path: AppRoutes.productDetailsView,
         builder: (context, state) => const ProductDetailsView(),
+      ),
+      GoRoute(
+        path: AppRoutes.cartView,
+        builder: (context, state) => const CartView(),
       ),
     ],
   );
