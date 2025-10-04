895e96d30d5077b474cd7c11c8d82220d1ef2238
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-04 15:03:20 +0300

    refactor

--- lib/features/product_details/views/product_details_view.dart
+++ lib/features/product_details/views/product_details_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:marketi/features/product_details/views/widgets/custom_product_details_bottom_section.dart';
+import 'package:marketi/features/product_details/views/widgets/custom_top_product_details_section.dart';
 
 class ProductDetailsView extends StatelessWidget {
   const ProductDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Details')),
+      body: const Stack(
+        children: [
+          CustomTopProductDetailsSection(),
+
+          Positioned(
+            bottom: 0,
+            child: CustomProductDetailsBottomSection(),
+          ),
+        ],
+      ),
+    );
   }
 }
