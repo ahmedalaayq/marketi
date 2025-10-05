7e880f0e2acaac015bc6b0311c2482270e720e42
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-05 17:50:52 +0300

    add function call back that accept categoryName to make request on home view

--- lib/features/home/views/widgets/custom_home_category_section.dart
+++ lib/features/home/views/widgets/custom_home_category_section.dart
@@ -3,10 +3,15 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:marketi/core/theme/app_text_style.dart';
 import 'package:marketi/core/theme/light_colors.dart';
 import 'package:marketi/features/home/models/category_model.dart';
-
 class CustomHomeCategorySection extends StatefulWidget {
-  const CustomHomeCategorySection({super.key, required this.categories});
+  const CustomHomeCategorySection({
+    super.key,
+    required this.categories,
+    required this.onCategorySelected,
+  });
+
   final List<CategoryModel> categories;
+  final Function(String categoryName) onCategorySelected; 
 
   @override
   State<CustomHomeCategorySection> createState() =>
@@ -15,14 +20,12 @@ class CustomHomeCategorySection extends StatefulWidget {
 
 class _CustomHomeCategorySectionState extends State<CustomHomeCategorySection> {
   int _selectedCategoryIndex = 0;
-  final testcategories = ["T-Shirts", "Jeans", "Shoes", "Jackets", "Accessories"];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 40.h,
       child: ListView.separated(
-        
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
@@ -33,6 +36,7 @@ class _CustomHomeCategorySectionState extends State<CustomHomeCategorySection> {
               setState(() {
                 _selectedCategoryIndex = index;
               });
+              widget.onCategorySelected(widget.categories[index].categoryName);
             },
             child: AnimatedContainer(
               curve: Curves.easeInOut,
@@ -40,23 +44,19 @@ class _CustomHomeCategorySectionState extends State<CustomHomeCategorySection> {
               alignment: Alignment.center,
               padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 7.h),
               decoration: BoxDecoration(
-                color:
-                    isSelected
-                        ? LightColors.primaryColor
-                        : LightColors.lightWhiteColor,
+                color: isSelected
+                    ? LightColors.primaryColor
+                    : LightColors.lightWhiteColor,
                 borderRadius: BorderRadius.circular(10.r),
-                boxShadow:
-                    isSelected
-                        ? [
-                          BoxShadow(
-                            color: LightColors.primaryColor.withValues(
-                              alpha: 0.3,
-                            ),
-                            blurRadius: 6,
-                            offset: const Offset(0, 3),
-                          ),
-                        ]
-                        : [],
+                boxShadow: isSelected
+                    ? [
+                        BoxShadow(
+                          color: LightColors.primaryColor.withValues(alpha: 0.3),
+                          blurRadius: 6,
+                          offset: const Offset(0, 3),
+                        ),
+                      ]
+                    : [],
               ),
               child: Text(
                 widget.categories[index].categoryName,
