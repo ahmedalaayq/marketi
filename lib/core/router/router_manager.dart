951bfc8ca1f1c227fe6e5fd3d8f2b8efb51ef3ac
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-05 17:51:14 +0300

    move bloc provider above screen route to save state and fix loading issue

--- lib/core/router/router_manager.dart
+++ lib/core/router/router_manager.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:marketi/core/router/app_routes.dart';
 import 'package:marketi/core/utils/service_locator.dart';
@@ -8,60 +9,85 @@ import 'package:marketi/features/auth/views/login_view.dart';
 import 'package:marketi/features/auth/views/sign_up_view.dart';
 import 'package:marketi/features/cart/views/address_view.dart';
 import 'package:marketi/features/cart/views/cart_view.dart';
+import 'package:marketi/features/home/cubits/home_categories_cubit.dart';
+import 'package:marketi/features/home/cubits/home_product_cubit.dart';
 import 'package:marketi/features/home/models/product_model.dart';
+import 'package:marketi/features/home/repo/home_repo.dart';
 import 'package:marketi/features/main/views/main_view.dart';
 import 'package:marketi/features/payment/views/payment_view.dart';
 import 'package:marketi/features/product_details/views/product_details_view.dart';
 
 class RouterManager {
-  static final GoRouter routerManager = GoRouter(
-    initialLocation: AppRoutes.loginView,
-    redirect: (context, state) async {
-      final token =
-          await di<StorageManager>().getToken(key: di<StorageKeys>().tokenKey)
-              as String?;
-      if (token != null && state.matchedLocation == AppRoutes.loginView ||
-          state.matchedLocation == AppRoutes.signUpView) {
-        return AppRoutes.mainView;
-      }
-      return null;
-    },
-    routes: <GoRoute>[
-      GoRoute(
-        path: AppRoutes.loginView,
-        builder: (context, state) => const LoginView(),
-      ),
-      GoRoute(
-        path: AppRoutes.signUpView,
-        builder: (context, state) => const SignUpView(),
-      ),
-      GoRoute(
-        path: AppRoutes.mainView,
-        builder: (context, state) => const MainView(),
-      ),
-      GoRoute(
-        path: AppRoutes.productDetailsView,
-        builder: (context, state) {
-          final product = state.extra as ProductModel?;
-          return ProductDetailsView(product: product);
-        },
-      ),
-      GoRoute(
-        path: AppRoutes.cartView,
-        builder: (context, state) => const CartView(),
-      ),
-      GoRoute(
-        path: AppRoutes.accountView,
-        builder: (context, state) => const AccountView(),
-      ),
-      GoRoute(
-        path: AppRoutes.addressView,
-        builder: (context, state) => const AddressView(),
-      ),
-      GoRoute(
-        path: AppRoutes.paymentView,
-        builder: (context, state) => const PaymentView(),
-      ),
-    ],
-  );
+static final GoRouter routerManager = GoRouter(
+  initialLocation: AppRoutes.loginView,
+  redirect: (context, state) async {
+    final token =
+        await di<StorageManager>().getToken(key: di<StorageKeys>().tokenKey)
+            as String?;
+    if (token != null &&
+        (state.matchedLocation == AppRoutes.loginView ||
+         state.matchedLocation == AppRoutes.signUpView)) {
+      return AppRoutes.mainView;
+    }
+    return null;
+  },
+  routes: <GoRoute>[
+    GoRoute(
+      path: AppRoutes.loginView,
+      builder: (context, state) => const LoginView(),
+    ),
+    GoRoute(
+      path: AppRoutes.signUpView,
+      builder: (context, state) => const SignUpView(),
+    ),
+    GoRoute(
+      path: AppRoutes.mainView,
+      builder: (context, state) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) {
+                final cubit = HomeProductCubit(homeRepo: di<HomeRepo>());
+                cubit.fetchAllProducts();
+                return cubit;
+              },
+            ),
+            BlocProvider(
+              create: (context) {
+                final cubit = HomeCategoryCubit(homeRepo: di<HomeRepo>());
+                cubit.fetchAllCategories();
+                return cubit;
+              },
+            ),
+          ],
+          child: const MainView(),
+        );
+      },
+    ),
+    GoRoute(
+      path: AppRoutes.productDetailsView,
+      builder: (context, state) {
+        final product = state.extra as ProductModel?;
+        return ProductDetailsView(product: product);
+      },
+    ),
+    GoRoute(
+      path: AppRoutes.cartView,
+      builder: (context, state) => const CartView(),
+    ),
+    GoRoute(
+      path: AppRoutes.accountView,
+      builder: (context, state) => const AccountView(),
+    ),
+    GoRoute(
+      path: AppRoutes.addressView,
+      builder: (context, state) => const AddressView(),
+    ),
+    GoRoute(
+      path: AppRoutes.paymentView,
+      builder: (context, state) => const PaymentView(),
+    ),
+  ],
+);
+
 }
--- lib/features/home/views/widgets/home_view_body.dart
+++ lib/features/home/views/widgets/home_view_body.dart
@@ -5,73 +5,57 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) {
-            final cubit = HomeProductCubit(homeRepo: di<HomeRepo>());
-            cubit.fetchAllProducts();
-            return cubit;
-          },
-        ),
-        BlocProvider(
-          create: (context) {
-            final cubit = HomeCategoryCubit(homeRepo: di<HomeRepo>());
-            cubit.fetchAllCategories();
-            return cubit;
-          },
-        ),
-      ],
-
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            const SliverToBoxAdapter(child: CustomHomeTopSection()),
-            BlocBuilder<HomeCategoryCubit, HomeCategoriesState>(
-              builder: (context, state) {
-                if (state is HomeCategorySuccessState) {
-                  
-                  return SliverToBoxAdapter(
-                    child: CustomHomeCategorySection(
-                      categories: state.categories,
-                    ),
-                  );
-                }else
-                {
-                  return const SliverToBoxAdapter(child: SizedBox.shrink());
-                }
-              },
-            ),
-            SliverPadding(padding: EdgeInsets.only(top: 24.h)),
-            BlocBuilder<HomeProductCubit, HomeProductState>(
-              builder: (context, state) {
-                if (state is HomeProductLoadingState) {
-                  return const SliverFillRemaining(
-                    hasScrollBody: false,
-                    child: Center(child: CircularProgressIndicator()),
-                  );
-                } else if (state is HomeProductSuccessState) {
-                  return CustomHomeProductSliverGrid(products: state.products);
-                } else if (state is HomeProductFailureState) {
-                  return SliverFillRemaining(
-                    hasScrollBody: false,
-                    child: Center(
-                      child: Text(
-                        state.errorMessage,
-                        style: AppTextStyle.semiBold16.copyWith(
-                          color: LightColors.redColor,
-                        ),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          const SliverToBoxAdapter(child: CustomHomeTopSection()),
+          BlocBuilder<HomeCategoryCubit, HomeCategoriesState>(
+            builder: (context, state) {
+              if (state is HomeCategorySuccessState) {
+                
+                return SliverToBoxAdapter(
+                  child: CustomHomeCategorySection(
+                    categories: state.categories,
+                    onCategorySelected: (categoryName) {
+                      context.read<HomeProductCubit>().fetchProductsByCategory(categoryName);
+                    },
+                  ),
+                );
+              }else
+              {
+                return const SliverToBoxAdapter(child: SizedBox.shrink());
+              }
+            },
+          ),
+          SliverPadding(padding: EdgeInsets.only(top: 24.h)),
+          BlocBuilder<HomeProductCubit, HomeProductState>(
+            builder: (context, state) {
+              if (state is HomeProductLoadingState) {
+                return const SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              } else if (state is HomeProductSuccessState) {
+                return CustomHomeProductSliverGrid(products: state.products);
+              } else if (state is HomeProductFailureState) {
+                return SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Center(
+                    child: Text(
+                      state.errorMessage,
+                      style: AppTextStyle.semiBold16.copyWith(
+                        color: LightColors.redColor,
                       ),
                     ),
-                  );
-                }
-                return const SliverToBoxAdapter(child: SizedBox());
-              },
-            ),
-          ],
-        ),
+                  ),
+                );
+              }
+              return const SliverToBoxAdapter(child: SizedBox());
+            },
+          ),
+        ],
       ),
     );
   }
