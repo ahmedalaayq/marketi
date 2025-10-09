3a06712aef1b9e1a54eb6c2c272cf7be31868d02
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-09 13:14:42 +0300

    refactor home categories cubit add all category

--- lib/features/home/cubits/home_categories_cubit.dart
+++ lib/features/home/cubits/home_categories_cubit.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marketi/features/home/cubits/home_categories_state.dart';
+import 'package:marketi/features/home/models/category_model.dart';
 import 'package:marketi/features/home/repo/home_repo.dart';
 
 class HomeCategoryCubit extends Cubit<HomeCategoriesState> {
-  HomeCategoryCubit({required this.homeRepo}) : super(HomeCategoryInitialState());
+  HomeCategoryCubit({required this.homeRepo})
+    : super(HomeCategoryInitialState());
   final HomeRepo homeRepo;
 
   fetchAllCategories() async {
@@ -14,6 +16,8 @@ class HomeCategoryCubit extends Cubit<HomeCategoriesState> {
         emit(HomeCategoryFailureState(errorMessage: error));
       },
       (categories) {
+        categories.insert(0, CategoryModel(categoryName: 'All'));
+
         emit(HomeCategorySuccessState(categories: categories));
       },
     );
