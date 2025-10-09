6c70ce0e997f36f24c7529e2dcad0e67c5986ac4
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-10-09 13:19:13 +0300

    refactor added bloc consumer to rebuild and listen cart cubit while add product on cart

--- lib/features/product_details/views/widgets/custom_product_details_bottom_section.dart
+++ lib/features/product_details/views/widgets/custom_product_details_bottom_section.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:marketi/core/theme/app_text_style.dart';
 import 'package:marketi/core/theme/light_colors.dart';
 import 'package:marketi/core/widgets/custom_btn_icon.dart';
+import 'package:marketi/features/cart/cubit/cart_cubit.dart';
+import 'package:marketi/features/cart/cubit/cart_state.dart';
+import 'package:marketi/features/home/models/product_model.dart';
 
 class CustomProductDetailsBottomSection extends StatelessWidget {
-  const CustomProductDetailsBottomSection({super.key, required this.price});
-  final num price;
+  const CustomProductDetailsBottomSection({
+    super.key,
+    required this.productModel,
+  });
+  final ProductModel productModel;
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +34,55 @@ class CustomProductDetailsBottomSection extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text('Price', style: AppTextStyle.regular16),
-                Text('\$$price', style: AppTextStyle.semiBold24),
+                Text('\$${productModel.price}', style: AppTextStyle.semiBold24),
               ],
             ),
             SizedBox(width: 16.w),
             Flexible(
-              child: CustomBtnIcon(
-                icon: Icons.shopping_cart,
-                btnText: 'Add to Cart',
-                onPressed: () {},
+              child: BlocConsumer<CartCubit, CartState>(
+                listener: (context, state) {
+                  if (state is AddingToCartSuccessState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        behavior: SnackBarBehavior.floating,
+                        backgroundColor: LightColors.primaryColor,
+                        content: Row(
+                          children: [
+                            Expanded(child: Text('Product Added Successfully')),
+                            Icon(Icons.check_circle, color: Colors.white),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else if (state is AddingToCartFailureState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        behavior: SnackBarBehavior.floating,
+                        backgroundColor: Colors.redAccent,
+                        content: Text(state.errorMessage),
+                      ),
+                    );
+                  }
+                },
+
+                builder: (context, state) {
+                  final isLoading = state is AddingToCartLoadingState;
+                  return CustomBtnIcon(
+                    isLoading: isLoading,
+                    icon: Icons.shopping_cart,
+                    btnText: 'Add to Cart',
+                    onPressed:
+                        isLoading
+                            ? () {}
+                            : () {
+                              context.read<CartCubit>().addToCart(
+                                date: DateTime.now().toString(),
+                                quantity: 1,
+                                product: productModel,
+                              );
+                            },
+                  );
+                },
               ),
             ),
           ],
